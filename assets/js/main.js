b0ae928541fc4b4cc59c0bf649f044802a1d7631
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-04-27 12:31:06 +0200

    jssnacks 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,10 @@ if (word_1.length > word_2.length) {
     console.log(word_1, word_2)
 } else console.log("Pari")
  */
-//Snack 3:
+
+
+
+/* //Snack 3:
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 var number_10 = Number(prompt("Insert a number"));
 var number_11 = Number(prompt("Insert a number"));
@@ -40,6 +43,37 @@ var number_17 = Number(prompt("Insert a number"));
 var number_18 = Number(prompt("Insert a number"));
 var number_19 = Number(prompt("Insert a number"));
 
+//Il programma stampa la somma di tutti i numeri inseriti. 
 var total = number_10 + number_11 + number_12 + number_13 + number_14 + number_15 + number_16 + number_17 + number_18 + number_19;
 console.log(total)
-//Il programma stampa la somma di tutti i numeri inseriti. 
+
+// correzione
+
+var number_10 = Number(prompt("Insert a number"));
+var sum = 0;
+for (var i = 0; i < 10; i++){
+    var user_number = Number(prompt("Insert a number"));
+    sum += user_number
+}
+console.log(sum);
+ */
+
+// Snack 4:
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+var userEmail = prompt("Scopri subito se puoi partecipare alla festa inserendo il tuo nome!");
+var invitedUsers = ["Davide", "Alessandra", "Miranda", "Franco", "Nicol Bolas"];
+
+var invited = false;
+var message;
+for (var i = 0; i = invitedUsers.length; i++){
+    var insertName = invitedUsers[i];
+    if (insertName == userEmail) {
+        invited = true;
+    }
+}
+
+if (invited) {
+    message = "Benvenuto alla festa del grande Gatsby";
+} else {
+    message = "Non sei ben accetto alla festa del Grande Gatsby"
+}
